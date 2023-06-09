4a311d4e382f9a116cf4e1efa1a5760a9309207f
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2023-06-08 18:06:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "prettygcode"
+plugin_identifier = "prettygcodetransparent"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_%s" % plugin_identifier
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-PrettyGCode"
+plugin_name = "OctoPrint-PrettyGCode-Transparent"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.2.4"
+plugin_version = "1.2.4.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Pretty GCode Visualizer"
+plugin_description = "Pretty GCode Visualizer with a Transparent Nozzle"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Kragrathea"
+plugin_author = "Kragrathea (echo-lalia)"
 
 # The plugin's author's mail address.
 plugin_author_email = "todo@example.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/Kragrathea/OctoPrint-PrettyGCode"
+plugin_url = "https://github.com/echo-lalia/OctoPrint-PrettyGCode-Redesign"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
